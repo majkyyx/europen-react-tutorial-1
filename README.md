a6ecded165d8671066edc84a89741adf7b954fa1
Jiří Hofman <hofman@ders.cz>
2015-10-04 16:06:44 +0200

    Figurky

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # EUROPEN REACT TUTORIAL
-
+1. Udelame sachovnici
+2. S figurkama
+3. umime sachonvici vyresetovat
 ## What will we be using?
-
+1. Figurky ASCII: https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
 ### node.js and dependencies
 1. Use nvm: https://github.com/creationix/nvm
 2. Install latest node: `nvm install v4.1.1`
-3. Grab the source: `git clone https://github.com/jankopriva/europen-react-tutorial`
+3. Grab the source: `git clone https://github.com/jirihofman/europen-react-tutorial`
 4. Install dependencies: `npm i`
--- index.js
+++ index.js
@@ -2,33 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Square(props) {
-  let {x,y} = props;  // nactu hodnoty z props
+  let {x,y} = props;  // nactu hodnoty z props. V renderovani ChessBoard posilam i props.piece (pro klikani, tim se nacte pismenko)
 
   // styly pro jedno policko
   var styles = {
     width: '12.5%',
     height: '12.5%',
     backgroundColor: 'black',
-    color: 'white'
+    color: 'white',
+    textAlign: 'center'
   };
 
   if (x%2 === y%2) {
-    styles.WebkitFilter = 'invert(100%)';
+    styles.WebkitFilter = 'invert(100%)'; // bily policko
   }
   let clickHandler = () => props.onSquareClicked(x,y);
-  return <div style={styles} onClick={clickHandler}>X</div>
+  return <div style={styles} onClick={clickHandler}>{props.piece}</div>
 }
 
+// Smart komponenta
 class ChessBoard extends React.Component {
+  constructor() {
+    super(...arguments); // musi to tam byt, jinak se to rozbije
+
+    this.state = {pieces: [] };
+  }
   togglePiece(x,y){
-    console.log('Toggle Piece [%s, %s]', x, y);
+    let pieces = this.state.pieces;
+    let index = (y * 8) + x;
+    pieces[index] = '♕';
+    this.setState({pieces})
+    console.log('Toggle Piece [%s, %s], objevi se neco: ', x, y, pieces[index]);
   }
 
   render() {
     let squares = [];
 
     for (var i = 0; i < 64; i++) {
-      squares.push(<Square key={i} x={i % 8} y={Math.floor(i/8)} onSquareClicked={this.togglePiece} />)
+      squares.push(<Square
+                    key={i} x={i % 8} y={Math.floor(i/8)}
+                    onSquareClicked={this.togglePiece.bind(this)} // v this muze byt uplne jiny objekt. Tim zajistim, ze this, kterej v Square pouzivam je prave ten ChessBoard.state.pieces
+                    piece={this.state.pieces[i]}
+                    />)
     }
 
     var styles = {
@@ -41,8 +56,8 @@ class ChessBoard extends React.Component {
   }
 }
 
+// zapojeni vseho nahore
 let rootElement = document.getElementById('root');
 ReactDOM.render(
-  <ChessBoard/>,
-  rootElement
+  <ChessBoard/>, rootElement
 );
